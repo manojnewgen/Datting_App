966f061e5608b2f5f7d7bed22097d91902780d37
manojnewgen <mannu9058@gmail.com>
2020-01-24 00:54:21 +0530

    "fixing conflict"

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,13 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-<<<<<<< HEAD
 import { BsDropdownModule } from 'ngx-bootstrap';
 import { RouterModule } from '@angular/router';
-=======
 
-import { BsDropdownModule } from 'ngx-bootstrap';
->>>>>>> eac61ab11a931a3469b5e274263f66c51bbdaa8d
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -18,13 +14,11 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterCeptorProvider } from './_services/error.interceptor';
 import { AlertifyService } from './_services/alertify.service';
-<<<<<<< HEAD
 import { MemberListComponent } from './member-list/member-list.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { appRoute } from './routes';
-=======
->>>>>>> eac61ab11a931a3469b5e274263f66c51bbdaa8d
+
 
 
 @NgModule({
@@ -40,18 +34,13 @@ import { appRoute } from './routes';
    imports: [
       BrowserModule,
       HttpClientModule,
-<<<<<<< HEAD
       FormsModule,
       // tousethemoduleinloginform\\\\r\\\\nBrowserAnimationsModule,
       BrowserAnimationsModule,
       BsDropdownModule.forRoot(),
       RouterModule.forRoot(appRoute, {}),
       //  RouterModule.forChild(appRoute)
-=======
-      FormsModule, // tousethemoduleinloginform
-      BrowserAnimationsModule,
-      BsDropdownModule.forRoot()
->>>>>>> eac61ab11a931a3469b5e274263f66c51bbdaa8d
+
    ],
    providers: [
       AuthService,
--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
-<<<<<<< HEAD
 import { Router } from '@angular/router';
-=======
->>>>>>> eac61ab11a931a3469b5e274263f66c51bbdaa8d
 
 @Component({
   selector: 'app-nav',
@@ -13,28 +10,17 @@ import { Router } from '@angular/router';
 })
 export class NavComponent implements OnInit {
 model: any = {};
-<<<<<<< HEAD
   constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router) { }
-=======
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
->>>>>>> eac61ab11a931a3469b5e274263f66c51bbdaa8d
 
   ngOnInit() {
   }
     login() {
-<<<<<<< HEAD
     this.authService.login(this.model).subscribe(next => {
       this.alertify.success('Logged in Successfully');
     },
     error => { this.alertify.error('Failed in login');
     }, () => this.router.navigate(['/members']) );
-=======
-    this.authService.login(this.model).subscribe(() => {
-      this.alertify.success('Logged in Successfully');
-    },
-    error => { this.alertify.error('Failed in login');
-    });
->>>>>>> eac61ab11a931a3469b5e274263f66c51bbdaa8d
+
   }
 
   loggedIn() {
@@ -43,10 +29,7 @@ model: any = {};
   logout() {
     const token = localStorage.removeItem('token');
     this.alertify.message('Logged out');
-<<<<<<< HEAD
     this.router.navigate(['/home']);
-=======
->>>>>>> eac61ab11a931a3469b5e274263f66c51bbdaa8d
   }
 
 }
